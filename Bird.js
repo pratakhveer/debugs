701a3faa36c657df041a7552a12528a6b6edd536
pratakhveer <pratakhveer1973@hotmail.com>
2021-01-13 19:51:51 +0530

    hello

--- Bird.js
+++ Bird.js
@@ -4,6 +4,7 @@ class Bird extends BaseClass {
     this.image = loadImage("sprites/bird.png");
     this.smokeImage = loadImage("sprites/smoke.png");
     this.trajectory =[];
+    this.visibility=255
   }
 
   display() {
@@ -19,7 +20,9 @@ class Bird extends BaseClass {
    
 
     for(var i=0; i<this.trajectory.length; i++){
+    
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+     
     }
   }
 }
